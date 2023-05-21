0403a14024de29dacfe98cb526d54c8b26f16d5a
luciogarcia02 <luciogarcialam@gmail.com>
2023-05-21 17:33:48 -0300

    funcionalidad eliminar personal

--- app/src/main/java/com/example/proyecto_final_oty_app/fragments/DetallePersonal.kt
+++ app/src/main/java/com/example/proyecto_final_oty_app/fragments/DetallePersonal.kt
@@ -6,13 +6,21 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
+import androidx.navigation.fragment.findNavController
 import com.example.proyecto_final_oty_app.R
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class DetallePersonal : Fragment() {
 
     lateinit var v : View
     lateinit var nombrePersonal : TextView
+    lateinit var eliminarBtn : Button
+    lateinit var viewModel: DetallePersonalViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -22,17 +30,34 @@ class DetallePersonal : Fragment() {
 
         v =  inflater.inflate(R.layout.fragment_detalle_personal, container, false)
         nombrePersonal = v.findViewById(R.id.nombrePersonalDetalle)
-
+        eliminarBtn = v.findViewById(R.id.eliminarBtn)
 
         return v
     }
 
-
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(DetallePersonalViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
     override fun onStart() {
         super.onStart()
         val personal = DetallePersonalArgs.fromBundle(requireArguments()).personal
         nombrePersonal.text = personal.nombre
 
+        eliminarBtn.setOnClickListener {
+            val idPersonal = personal.id // Aquí deberías obtener el id que quieres eliminar.
+            CoroutineScope(Dispatchers.Main).launch {
+                try{
+                    viewModel.eliminarPersonal(idPersonal)
+                    Toast.makeText(context, "Personal $idPersonal eliminado correctamente", Toast.LENGTH_SHORT).show()
+
+                    findNavController().popBackStack()
+                }catch(e:Exception){
+                    Toast.makeText(context, "Error al eliminar el equipo: $e", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
     }
 
 }
--- app/src/main/java/com/example/proyecto_final_oty_app/fragments/DetallePersonalViewModel.kt
+++ app/src/main/java/com/example/proyecto_final_oty_app/fragments/DetallePersonalViewModel.kt
@@ -1,7 +1,20 @@
 package com.example.proyecto_final_oty_app.fragments
 
 import androidx.lifecycle.ViewModel
+import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.tasks.await
+import kotlinx.coroutines.withContext
 
 class DetallePersonalViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+    private val firestore = FirebaseFirestore.getInstance()
+    private val personalCollection = firestore.collection("personal")
+
+
+    suspend fun eliminarPersonal(idPersonal: String?) = withContext(Dispatchers.IO) {
+        if(idPersonal!=null){
+            val personalDocument = personalCollection.document(idPersonal)
+            personalDocument.delete().await()
+        }
+    }
 }
